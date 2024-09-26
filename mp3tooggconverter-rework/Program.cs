34c34a02139c801d2be17871e38ea97411b9db54
Kociarz <dbednorz@tuta.io>
2024-09-26 20:58:52 +0200

    Add project files.

---
+++ mp3tooggconverter-rework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mp3tooggconverter_rework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
